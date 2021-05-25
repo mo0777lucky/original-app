dcc3b0c4a0d20f4e67088ce60a8e77d1f7e2fd01
mo0777lucky <beautifulsea08264@gmail.com>
2021-05-25 14:59:11 +0900

    コメント機能作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to:"products#index"
   resources :products, only:[:new, :create, :show, :edit, :update, :destroy]do
+    resources :comments, only: :create
 end
   resources :users, only: :show
 end
